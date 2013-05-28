294f4cdd324580ce05e4e4a04ff1850c4769dbbf
Andy Cheung <win120a@163.com>
2013-05-28 19:11:52 +0800

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,9 @@
 <script>
 versign = "?vs=ac";
 if(location.search != versign){
+  if(location.search == '?vs=forget'){
+    location.href = 'reset.php';
+  }
   alert('Error!');
   history.back();
 }
