d91be6c8c64014f968e77c938b5588547162199b
Andy Cheung <win120a@163.com>
2013-05-30 20:25:48 +0800

    Update reset.php

--- reset.php
+++ reset.php
@@ -1,4 +1,14 @@
 <script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?235d9813ec06f146ab752389d01f3492";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
+<script>
   document.title = "Andy Site Login Reset Page";
   sc = prompt('Question:\nMy GitHub Address:');
   if(sc != 'http://github.com/win120a'){alert('Error!');history.back();}
