e9da066c9f1e03755c552d3937323ad2416a5078
Andy Cheung <win120a@163.com>
2013-05-30 20:41:11 +0800

    Update reset.php

--- reset.php
+++ reset.php
@@ -12,5 +12,5 @@ var _hmt = _hmt || [];
   document.title = "Andy Site Login Reset Page";
   sc = prompt('Question:\nMy GitHub Address:');
   if(sc != 'http://github.com/win120a'){alert('Error!');history.back();}
-  else{alert('Key is ac');location.href='login.php';}
+  else{alert('Key is ac');location.href='login.php?skiplogin';}
 </script>
