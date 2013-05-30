9c3ff93a3b8af7901dcde3126a7b93fb99f4a4cc
Andy Cheung <win120a@163.com>
2013-05-30 21:25:19 +0900

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,14 @@
 <script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?235d9813ec06f146ab752389d01f3492";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
+<script>
   document.title = "Andy Site Login Page";
   sc = prompt('Product Key','');
   location.href = './verify.php?vs=' + sc;
