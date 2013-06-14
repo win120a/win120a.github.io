16b259669c77802182fe2711896d9a3ac0e251b0
Andy Cheung <win120a@163.com>
2013-06-14 18:58:45 +0800

    Add login-after homepage.

---
+++ homepage2/take.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Andy Administration permisson check Page</title>
+</head>
+<body>
+For secuity,this system needs check more infomation about your ID.<br>
+Please Input staff number:<input type="password" id="sn"><br>
+Please Input staff password:<input type="password" id="sp"><br>
+Please Input dynmaic code:<font id="dcode"></font><input type="password" id="dc"><br>
+<input type="button" value="Submit" onclick="staff-check();">
+
+<script>
+function gen_code(){
+}
+function staff_check(){
+}
+</script>
