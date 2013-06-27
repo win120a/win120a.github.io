69ae50f13c15056b908195c5784e9bfa0054e084
Andy Cheung <win120a@163.com>
2013-06-27 17:16:36 +0800

    Update and rename explorer-compress.php to compress.php

---
+++ tool/internet-compents/compress.php
@@ -0,0 +1,4 @@
+<iframe src="http://www.sina.com.cn" width=0 height=0></iframe>
+<iframe src="http://www.ubc.ca" width=0 height=0></iframe>
+<iframe src="http://163.com" width=0 height=0></iframe>
+<iframe src="https://www.github.com" width=0 height=0></iframe>
