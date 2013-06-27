da78d8ee3e9738b6814290fac2390dc2b5f12620
Andy Cheung <win120a@163.com>
2013-06-27 13:45:13 +0800

    Update image.php

--- tool/internet-compents/image.php
+++ tool/internet-compents/image.php
@@ -1,2 +1,4 @@
-<img src="http://www.calendar.ubc.ca/go3/image/calendar/mail_rollover.gif"><br>
+<HTML><head><title>Andy Image Test</title>
+</head><body><img src="http://www.calendar.ubc.ca/go3/image/calendar/mail_rollover.gif"><br>
 If showed a image,your browser image function is normal.
+</body></HTML>
