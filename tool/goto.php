330d107e79773e22f92c1f1f187d9a87cebbb736
Andy Cheung <win120a@163.com>
2013-06-30 20:50:59 +0800

    Update goto.php

--- tool/goto.php
+++ tool/goto.php
@@ -1,4 +1,14 @@
 <script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?235d9813ec06f146ab752389d01f3492";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
+<script>
 if(location.search == ''){history.back();}
 location.href = location.search;
 </script>
