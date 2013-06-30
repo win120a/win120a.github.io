8c604e66381cd30345cda030d249faaf3cdfeb93
Andy Cheung <win120a@163.com>
2013-06-30 20:56:51 +0800

    Update compress.php

--- tool/internet-compents/compress.php
+++ tool/internet-compents/compress.php
@@ -1,4 +1,18 @@
-<html><head><title>Andy Browser compress tool</title><body>
+<html>
+<head>
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?235d9813ec06f146ab752389d01f3492";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
+<title>Andy Browser compress tool</title>
+</head>
+<body>
 <iframe src="http://www.sina.com.cn" width=0 height=0></iframe>
 <iframe src="http://www.ubc.ca" width=0 height=0></iframe>
 <iframe src="http://163.com" width=0 height=0></iframe>
