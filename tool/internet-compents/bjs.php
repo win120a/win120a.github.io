3d3dca40b63feb89a4615c0da4a0ca026589ce42
Andy Cheung <win120a@163.com>
2013-06-30 20:55:02 +0800

    Update bjs.php

--- tool/internet-compents/bjs.php
+++ tool/internet-compents/bjs.php
@@ -1,4 +1,19 @@
-<html><head><title>Basic JS Test</title></head><body>
+<html>
+<head>
+
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?235d9813ec06f146ab752389d01f3492";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+<title>Basic JS Test</title>
+</head>
+
+<body>
 <a href="javascript:test();">Test!</a>
 <br><br>
 Process:<br>
